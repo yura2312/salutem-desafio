6f564d487af1a171a977d887e058a32b62b04193
Gabriel Daiki <daiki.gab@gmail.com>
2026-09-02 15:22:00 -0300

    config: adicionar testcontainers para testar a camada de repositorio com o banco postgresql

---
+++ backend/src/test/java/com/lanchonete/salutem/TestContainerConfiguration.java
@@ -0,0 +1,19 @@
+package com.lanchonete.salutem;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestContainerConfiguration {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer postgresContainer() {
+        return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+    }
+
+
+}
