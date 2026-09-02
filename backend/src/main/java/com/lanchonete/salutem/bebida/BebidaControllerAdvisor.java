8e205a4d42b2ac4f96e09397a8ac5d9817c8c6db
Gabriel Daiki <daiki.gab@gmail.com>
2026-09-02 17:28:02 -0300

    feat: adicionar controller advice para lidar com as excecoes lancadas

---
+++ backend/src/main/java/com/lanchonete/salutem/bebida/BebidaControllerAdvisor.java
@@ -0,0 +1,18 @@
+package com.lanchonete.salutem.bebida;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class BebidaControllerAdvisor {
+
+    @ExceptionHandler(BebidaNotFoundException.class)
+    public ResponseEntity<ProblemDetail> bebidaNotFoundExceptionHandler(BebidaNotFoundException ex) {
+        var problem = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problem.setTitle("Bebida não encontrada");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problem);
+    }
+}
