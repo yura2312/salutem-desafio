c4cb753a10e3981a3cd118293b7da026e327efdb
Gabriel Daiki <daiki.gab@gmail.com>
2026-09-02 17:41:39 -0300

    chore: remover import nao utilizado

--- backend/src/main/java/com/lanchonete/salutem/bebida/BebidaService.java
+++ backend/src/main/java/com/lanchonete/salutem/bebida/BebidaService.java
@@ -5,7 +5,6 @@ import com.lanchonete.salutem.bebida.model.dto.BebidaResponse;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class BebidaService {
