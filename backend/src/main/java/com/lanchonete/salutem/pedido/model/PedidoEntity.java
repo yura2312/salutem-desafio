020c1c3cbb5e335d625e28235deb17a4743f5c3d
Gabriel Daiki <daiki.gab@gmail.com>
2026-09-03 13:28:32 -0300

    feat: adicionar entidade pedido

---
+++ backend/src/main/java/com/lanchonete/salutem/pedido/model/PedidoEntity.java
@@ -0,0 +1,55 @@
+package com.lanchonete.salutem.pedido.model;
+
+import com.lanchonete.salutem.bebida.model.BebidaEntity;
+import com.lanchonete.salutem.hamburguer.model.HamburguerEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "pedido")
+public class PedidoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Instant data;
+
+    private String descricao;
+
+    private String cliente_nome;
+
+    private String cliente_endereco;
+
+    private String cliente_telefone;
+
+    @ManyToMany
+    @JoinTable(
+            name = "pedido_hamburguer",
+            joinColumns = @JoinColumn(name = "id_pedido"),
+            inverseJoinColumns = @JoinColumn(name = "id_hamburguer")
+    )
+    private Set<HamburguerEntity> hamburgueres = new HashSet<>();
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "pedido_bebida",
+            joinColumns = @JoinColumn(name = "id_pedido"),
+            inverseJoinColumns = @JoinColumn(name = "id_bebida")
+    )
+    private Set<BebidaEntity> bebidas = new HashSet<>();
+
+}
