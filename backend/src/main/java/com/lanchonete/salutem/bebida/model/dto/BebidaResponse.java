a18f7ba86a3e3d0ae84a62d902ec10667b27f553
Gabriel Daiki <daiki.gab@gmail.com>
2026-09-03 01:41:17 -0300

    fix: dto bebida resposta retorna id

--- backend/src/main/java/com/lanchonete/salutem/bebida/model/dto/BebidaResponse.java
+++ backend/src/main/java/com/lanchonete/salutem/bebida/model/dto/BebidaResponse.java
@@ -3,6 +3,7 @@ package com.lanchonete.salutem.bebida.model.dto;
 import java.math.BigDecimal;
 
 public record BebidaResponse(
+        Long id,
         String descricao,
         BigDecimal precoUnitario,
         Boolean contemAcucar) {
