a1995c47801c0d12ff0db3c9355edeae5d606a4d
Gabriel Daiki <daiki.gab@gmail.com>
2026-09-04 00:53:55 -0300

    feat: adicionar classe da excecao lista hamburguer e pedido nao podem ser nulas

---
+++ backend/src/main/java/com/lanchonete/salutem/pedido/PedidoBebidaAndHamburguerEmptyException.java
@@ -0,0 +1,7 @@
+package com.lanchonete.salutem.pedido;
+
+public class PedidoBebidaAndHamburguerEmptyException extends RuntimeException {
+    public PedidoBebidaAndHamburguerEmptyException(String message) {
+        super(message);
+    }
+}
