396906877b71bca4c4a02491a23d9210f58b478a
Gabriel Daiki <daiki.gab@gmail.com>
2026-09-04 00:30:57 -0300

    feat: adicionar validacoes no dtoRequest pedido

--- backend/src/main/java/com/lanchonete/salutem/pedido/model/dto/PedidoRequest.java
+++ backend/src/main/java/com/lanchonete/salutem/pedido/model/dto/PedidoRequest.java
@@ -1,14 +1,29 @@
 package com.lanchonete.salutem.pedido.model.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import java.util.List;
 
 public record PedidoRequest(
+        @NotNull(message = "Descrição não pode ser nula")
         String descricao,
+        @NotNull(message = "Nome do cliente não pode ser nulo")
         String clienteNome,
+        @NotNull(message = "Endereço do cliente não pode ser nulo")
         String clienteEndereco,
+        @NotNull(message = "Telefone do cliente não pode ser nulo")
+        @Length(min = 10, max = 11, message = "Telefone do cliente deve ter entre 10 e 11 dígitos")
         String clienteTelefone,
-        List<Long> idHamburgueres,
-        List<Long> idBebidas,
+        List<@Positive(message = "A lista de ids de hamburgueres não pode conter valores negativos")
+                Long> idHamburgueres,
+        List<@Positive(message = "A lista de ids de bebidas não pode conter valores negativos")
+                Long> idBebidas,
+
         String observacoes
 ) {
 }
