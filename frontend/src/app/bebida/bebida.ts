1f6496b50c41a545acba7ab9b7fe6ccccf24e887
Gabriel Daiki <daiki.gab@gmail.com>
2026-09-04 23:28:39 -0300

    feat: adicionar error handler para respostas do backend

--- frontend/src/app/bebida/bebida.ts
+++ frontend/src/app/bebida/bebida.ts
@@ -5,20 +5,28 @@ import { ApiBebida, BebidaRequest, BebidaResponse } from '../service/api-bebida'
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatButton } from '@angular/material/button';
+import { MatOption } from '@angular/material/autocomplete';
+import { MatSelect } from '@angular/material/select';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialog } from '../shared/error-dialog/error-dialog';
 
 @Component({
-  imports: [FormField,
+  imports: [
+    FormField,
     MatFormField,
     MatLabel,
     MatInput,
     MatCheckbox,
-    MatButton],
+    MatButton,
+    MatSelect,
+    MatOption,
+  ],
   selector: 'app-bebida',
   templateUrl: './bebida.html',
 })
 export class Bebida implements OnInit {
-
   private api = inject(ApiBebida);
+  private readonly dialog = inject(MatDialog);
 
   bebidas = signal<BebidaResponse[]>([]);
 
@@ -27,16 +35,15 @@ export class Bebida implements OnInit {
   bebidaModel = signal<BebidaRequest>({
     descricao: '',
     precoUnitario: 0,
-    contemAcucar: false
+    contemAcucar: false,
   });
 
   bebidaForm = form(this.bebidaModel);
 
-  ultimasBebidas = computed(() =>
-    [...this.bebidas()]
-      .sort((a, b) => b.id - a.id)
-      .slice(0, 5)
-  );
+  ultimasBebidas = computed(() => [...this.bebidas()].sort((a, b) => b.id - a.id).slice(0, 5));
+
+  buscaModel = signal({ termo: '', tipo: 'id' as 'id' | 'descricao' });
+  buscaForm = form(this.buscaModel);
 
   ngOnInit(): void {
     this.carregarBebidas();
@@ -44,12 +51,14 @@ export class Bebida implements OnInit {
 
   carregarBebidas(): void {
     this.api.getAll().subscribe({
-      next: bebidas => {
+      next: (bebidas) => {
         this.bebidas.set(bebidas);
       },
-      error: erro => {
-        console.error('Erro ao carregar bebidas', erro);
-      }
+      error: (error) => {
+        this.dialog.open(ErrorDialog, {
+          data: error.error,
+        });
+      },
     });
   }
 
@@ -58,25 +67,33 @@ export class Bebida implements OnInit {
 
     if (id === null) {
       this.api.save(this.bebidaModel()).subscribe({
-        next: bebida => {
-          this.bebidas.update(lista => [...lista, bebida]);
+        next: (bebida) => {
+          this.bebidas.update((lista) => [...lista, bebida]);
           this.limparFormulario();
-        }
+        },
+        error: (error) => {
+          this.dialog.open(ErrorDialog, {
+            data: error.error,
+          });
+        },
       });
 
       return;
     }
 
     this.api.update(id, this.bebidaModel()).subscribe({
-      next: bebidaAtualizada => {
-        this.bebidas.update(lista =>
-          lista.map(bebida =>
-            bebida.id === id ? bebidaAtualizada : bebida
-          )
+      next: (bebidaAtualizada) => {
+        this.bebidas.update((lista) =>
+          lista.map((bebida) => (bebida.id === id ? bebidaAtualizada : bebida)),
         );
 
         this.limparFormulario();
-      }
+      },
+      error: (error) => {
+        this.dialog.open(ErrorDialog, {
+          data: error.error,
+        });
+      },
     });
   }
 
@@ -86,17 +103,20 @@ export class Bebida implements OnInit {
     this.bebidaModel.set({
       descricao: bebida.descricao,
       precoUnitario: bebida.precoUnitario,
-      contemAcucar: bebida.contemAcucar
+      contemAcucar: bebida.contemAcucar,
     });
   }
 
   deletar(id: number): void {
     this.api.delete(id).subscribe({
       next: () => {
-        this.bebidas.update(lista =>
-          lista.filter(bebida => bebida.id !== id)
-        );
-      }
+        this.bebidas.update((lista) => lista.filter((bebida) => bebida.id !== id));
+      },
+      error: (error) => {
+        this.dialog.open(ErrorDialog, {
+          data: error.error,
+        });
+      },
     });
   }
 
@@ -110,7 +130,46 @@ export class Bebida implements OnInit {
     this.bebidaModel.set({
       descricao: '',
       precoUnitario: 0,
-      contemAcucar: false
+      contemAcucar: false,
     });
+  }
+
+  pesquisar(): void {
+    const busca = this.buscaModel();
+
+    if (!busca.termo.trim()) {
+      this.carregarBebidas();
+      return;
+    }
+
+    if (busca.tipo === 'id') {
+      this.api.get(Number(busca.termo)).subscribe({
+        next: (bebida) => {
+          this.bebidas.set([bebida]);
+        },
+        error: (error) => {
+          this.dialog.open(ErrorDialog, {
+            data: error.error,
+          });
+        },
+      });
+      return;
+    }
+
+    this.api.getByDescricao(busca.termo).subscribe({
+      next: (bebida) => {
+        this.bebidas.set(bebida);
+      },
+      error: (error) => {
+        this.dialog.open(ErrorDialog, {
+          data: error.error,
+        });
+      },
+    });
+  }
+  verificarBarraDeBusca(): void {
+    if (this.buscaModel().termo.trim() === '') {
+      this.carregarBebidas();
+    }
   }
 }
